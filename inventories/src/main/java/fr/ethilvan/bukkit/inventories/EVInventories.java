7e1b43ee9256efad07fcd0f4184d428ed9c632ac
aumgn <aumgn@free.fr>
2013-02-11 15:04:33 +0100

    [Inventories] Clean up

---
+++ inventories/src/main/java/fr/ethilvan/bukkit/inventories/EVInventories.java
@@ -0,0 +1,50 @@
+package fr.ethilvan.bukkit.inventories;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.avaje.ebean.EbeanServer;
+
+import fr.ethilvan.bukkit.api.inventories.Inventories;
+
+public class EVInventories implements Inventories {
+
+    private final InventoriesPlugin plugin;
+
+    public EVInventories(InventoriesPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void save(Player player, String description) {
+        EbeanServer db = plugin.getDatabase();
+        Inventory inv = db.find(Inventory.class)  
+                .where().eq("description", description).findUnique();
+        if (inv == null) {
+            inv = new Inventory();
+            inv.setDescription(description);
+        }
+        else { db.delete(inv.getSlots()); }
+        inv.update(db, player.getInventory());
+        db.save(inv);
+    }
+
+    @Override
+    public void set(Player player, String description) {
+        EbeanServer db = plugin.getDatabase();
+        Inventory inv = db.find(Inventory.class)  
+                .where().eq("description", description).findUnique();
+        if (inv == null) {
+            player.getInventory().setContents(new ItemStack[36]);
+            player.getInventory().setArmorContents(new ItemStack[4]);
+        } else {
+            inv.setToPlayer(player);
+        }
+    }
+
+    @Override
+    public void replace(Player player, String from, String to) {
+        save(player, from);
+        set(player, to);
+    }
+}
--- inventories/src/main/java/fr/ethilvan/bukkit/inventories/InventoriesPlugin.java
+++ inventories/src/main/java/fr/ethilvan/bukkit/inventories/InventoriesPlugin.java
@@ -5,16 +5,12 @@ import java.util.List;
 
 import javax.persistence.PersistenceException;
 
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import com.avaje.ebean.EbeanServer;
 
 import fr.ethilvan.bukkit.api.EthilVan;
 import fr.ethilvan.bukkit.api.inventories.Inventories;
 
-public class InventoriesPlugin extends JavaPlugin implements Inventories {
+public class InventoriesPlugin extends JavaPlugin {
 
     @Override
     public List<Class<?>> getDatabaseClasses() {
@@ -23,11 +19,10 @@ public class InventoriesPlugin extends JavaPlugin implements Inventories {
         list.add(Inventory.class);
         list.add(Enchantment.class);
         return list;
-    }   
+    }
 
     @Override
     public void onEnable() {
-        EthilVan.registerInventories(this);
         try {
             getDatabase().find(Slot.class).findRowCount();
             getDatabase().find(Inventory.class).findRowCount();
@@ -35,38 +30,8 @@ public class InventoriesPlugin extends JavaPlugin implements Inventories {
         } catch (PersistenceException exc) {
             installDDL();
         }
-    }
 
-    @Override
-    public void save(Player player, String description) {
-        EbeanServer db = getDatabase();
-        Inventory inv = db.find(Inventory.class)  
-                .where().eq("description", description).findUnique();
-        if (inv == null) {
-            inv = new Inventory();
-            inv.setDescription(description);
-        }
-        else { db.delete(inv.getSlots()); }
-        inv.update(db, player.getInventory());
-        db.save(inv);
-    }
-
-    @Override
-    public void set(Player player, String description) {
-        EbeanServer db = getDatabase();
-        Inventory inv = db.find(Inventory.class)  
-                .where().eq("description", description).findUnique();
-        if (inv == null) {
-            player.getInventory().setContents(new ItemStack[36]);
-            player.getInventory().setArmorContents(new ItemStack[4]);
-        } else {
-            inv.setToPlayer(player);
-        }
-    }
-
-    @Override
-    public void replace(Player player, String from, String to) {
-        save(player, from);
-        set(player, to);
+        Inventories inventories = new EVInventories(this);
+        EthilVan.registerInventories(inventories);
     }
 }
