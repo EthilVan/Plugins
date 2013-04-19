87c56f7ff57c51bf9b2ecc10c539491ef542a3f1
noogotte <hugotte.76@live.fr>
2013-04-19 22:58:15 +0200

    [Deaths] Only show how to make "a by DamageCause" or "a by mob" message

--- deaths/src/main/java/fr/ethilvan/bukkit/deaths/DeathMessages.java
+++ deaths/src/main/java/fr/ethilvan/bukkit/deaths/DeathMessages.java
@@ -13,36 +13,10 @@ public class DeathMessages {
         fireMessages.add("<player> a fini en cendres.");
         fireMessages.add("<player> a eu chaud, très chaud, trop chaud...");
         this.deathMessages.add(new DeathMessage("FIRE", fireMessages));
-        List<String> lavaMessages = new ArrayList<String>();
-        lavaMessages.add("<player> a fini dans la lave...");
-        lavaMessages.add("<player> s'est transformé en Obsidienne.");
-        lavaMessages.add("<player> a pris un bain dans un lac de feu.");
-        this.deathMessages.add(new DeathMessage("LAVA", lavaMessages));
         List<String> zombieMessages = new ArrayList<String>();
         zombieMessages.add("<player> s'est fait dévorer vivant par un zombie.");
         zombieMessages.add("<player> a été tué par un zombie.");
         zombieMessages.add("<player> a servi de repas à un zombie.");
-        this.deathMessages.add(new DeathMessage("ZOMBIE", zombieMessages));
-        List<String> skeletonMessages = new ArrayList<String>();
-        skeletonMessages.add("<player> s'est fait cribler de flèches par un squelette.");
-        skeletonMessages.add("Un squelette a tué <player>.");
-        skeletonMessages.add("<player> a une flèche dans le front.");
-        this.deathMessages.add(new DeathMessage("SKELETON", skeletonMessages));
-        List<String> creeperMessages = new ArrayList<String>();
-        creeperMessages.add("<player> a été explosé par un Creeper.");
-        creeperMessages.add("<player> a essayé de faire un câlin à un Creeper.");
-        creeperMessages.add("SSSSSSssssssss...... est la derniere chose que <player> a entendu.");
-        this.deathMessages.add(new DeathMessage("CREEPER", creeperMessages));
-        List<String> explosionMessages = new ArrayList<String>();
-        explosionMessages.add("<player> a apparemment un caractère explosif.");
-        explosionMessages.add("<player> a explosé.");
-        explosionMessages.add("C'est ça de jouer avec la TNT <player>.");
-        this.deathMessages.add(new DeathMessage("BLOCK_EXPLOSION", explosionMessages));
-        List<String> playersMessages = new ArrayList<String>();
-        playersMessages.add("<murderer> a tué <player>.");
-        playersMessages.add("<player> a été surclassé par <murderer>.");
-        playersMessages.add("<murderer> a dominé <player>.");
-        this.deathMessages.add(new DeathMessage("PLAYER", playersMessages));
     }
 
     public List<DeathMessage> getDeathMessages() {
